e54a46307c7039ce193eed9292412ee55dccd194
kchoudhary2106 <kingshuk.choudhary@osttra.com>
2025-02-17 20:30:54 +0530

    16-02-25

--- src/basics/Patterns.java
+++ src/basics/Patterns.java
@@ -245,8 +245,130 @@ public class Patterns {
         }
     }
 
+    public static void p16(int n) {
+        int is = (2 * n - 3), star = 2;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= star / 2; j++) System.out.print(j + "\t");
+            for (int j = 1; j <= is; j++) System.out.print("_\t");
+            if (i == n) star -= 1;
+            for (int j = star / 2; j >= 1; j--) System.out.print(j + "\t");
+            is -= 2;
+
+            star += 2;
+            System.out.println();
+        }
+    }
+
+
+    public static void p17(int n) {
+        int os = 2, star = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= os; j++) {
+                if (i == n / 2 + 1) System.out.print("*\t");
+                else
+                    System.out.print("_\t");
+            }
+            for (int j = 1; j <= star; j++) System.out.print("*\t");
+
+            if (i <= n / 2)
+                star += 2;
+            else star -= 2;
+            System.out.println();
+        }
+    }
+
+    public static void p18(int n) {
+        int os = 0, star = n;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= os; j++) System.out.print("_\t");
+            for (int j = 1; j <= star; j++) {
+                if (i <= n / 2) {
+                    if (j == 1 || j == star || i == 1)
+                        System.out.print("*\t");
+                    else System.out.print("_\t");
+                } else {
+                    System.out.print("*\t");
+                }
+            }
+            if (i <= n / 2) {
+                star -= 2;
+                os += 1;
+            } else {
+                star += 2;
+                os -= 1;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void p19(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j == 1 || j == n)
+                    System.out.print("*\t");
+                else if (i > n / 2 && (i + j == n + 1 || i == j)) {
+                    System.out.print("*\t");
+                } else System.out.print("_\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void p20(int n) {
+        int os = 1, star = n / 2 + 1, startingNum = 1;
+        for (int i = 1; i <= n; i++) {
+            int temp = startingNum;
+            for (int j = 1; j <= star; j++) {
+                System.out.print(temp + "\t");
+                temp++;
+            }
+            for (int j = 1; j <= os; j++) System.out.print("_\t");
+            temp--;
+            for (int j = star; j >= 1; j--) {
+                System.out.print(temp + "\t");
+                temp--;
+            }
+            if (i <= n / 2) {
+                star--;
+                os += 2;
+                startingNum++;
+            } else {
+                star++;
+                startingNum--;
+                os -= 2;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void p21(int n) {
+        int os = n, star = 1;
+        for (int i = 1; i <= 2 * n + 1; i++) {
+            for (int j = 1; j <= os; j++) System.out.print("_\t");
+            for (int j = 1; j <= star; j++) {
+                if (i == n + 1) {
+                    if (j == 1 || j == star) System.out.print("*\t");
+                    else System.out.print("_\t");
+                } else {
+                    if (j == star / 2 + 1 && i != 1 && i != 2 * n + 1)
+                        System.out.print("_\t");
+                    else System.out.print("*\t");
+                }
+            }
+            if (i <= n) {
+                star += 2;
+                os--;
+            } else {
+                os++;
+                star -= 2;
+            }
+            System.out.println();
+
+        }
+    }
 
     public static void main(String[] args) {
-        p15(2);
+        p21(4);
     }
 }
