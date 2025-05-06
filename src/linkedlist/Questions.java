87bd753ac599156f52d4a3d26ad096c9ff573706
kchoudhary2106 <kingshuk.choudhary@osttra.com>
2025-05-06 19:54:15 +0530

    03-05-2025

--- src/linkedlist/Questions.java
+++ src/linkedlist/Questions.java
@@ -130,6 +130,15 @@ public class Questions {
         }
     }
 
+    private void addLast(ListNode node) {
+        if (th == null) {
+            th = tt = node;
+        } else {
+            tt.next = node;
+            tt = node;
+        }
+    }
+
     private int sizeLL(ListNode head) {
         int len = 0;
         while (head != null) {
@@ -244,4 +253,187 @@ public class Questions {
         }
         return false;
     }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null || list2 == null) {
+            return list1 == null ? list2 : list1;
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+
+        while (list1 != null && list2 != null) {
+            int v1 = list1.val;
+            int v2 = list2.val;
+            if (v1 < v2) {
+                prev.next = list1;
+                prev = list1;
+                list1 = list1.next;
+            } else {
+                prev.next = list2;
+                prev = list2;
+                list2 = list2.next;
+            }
+        }
+        if (list1 == null) {
+            prev.next = list2;
+        } else {
+            prev.next = list1;
+        }
+        return dummy.next;
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tail = headA;
+        ListNode temp = headA;
+        while (temp != null) {
+            tail = temp;
+            temp = temp.next;
+        }
+        tail.next = headA;
+
+        ListNode slow = headB, fast = headB;
+        boolean flag = false;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            tail.next = null;
+            return null;
+        }
+        slow = headB;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        tail.next = null;
+        return slow;
+    }
+
+    class BrowserHistory {
+        class ListNode {
+            String data;
+            ListNode prev;
+            ListNode next;
+
+            public ListNode(String data) {
+                this.data = data;
+            }
+
+        }
+
+        private ListNode curr;
+
+        public BrowserHistory(String homepage) {
+            ListNode newNode = new ListNode(homepage);
+            curr = newNode;
+        }
+
+        public void visit(String url) {
+            ListNode newNode = new ListNode(url);
+            curr.next = newNode;
+            newNode.prev = curr;
+            curr = newNode;
+        }
+
+        public String back(int steps) {
+            while (steps-- > 0 && curr.prev != null) {
+                curr = curr.prev;
+            }
+            return curr.data;
+        }
+
+        public String forward(int steps) {
+            while (steps-- > 0 && curr.next != null) {
+                curr = curr.next;
+            }
+            return curr.data;
+        }
+    }
+
+    public ListNode reverseEvenLengthGroups(ListNode head) {
+        int groupSize = 1;
+        int size = sizeLL(head);
+        while (size >= groupSize) {
+            int tempSize = groupSize;
+            while (tempSize-- > 0) {
+                ListNode next = head.next;
+                ListNode curr = head;
+                head.next = null;
+                if (groupSize % 2 == 0) {
+                    addFirst(curr);
+                } else {
+                    addLast(curr);
+                }
+                head = next;
+            }
+            if (oh == null) {
+                oh = th;
+                ot = tt;
+            } else {
+                ot.next = th;
+                ot = tt;
+            }
+            tt = th = null;
+            size -= groupSize;
+            groupSize++;
+        }
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = null;
+            if (size % 2 == 0) {
+                addFirst(head);
+            } else {
+                addLast(head);
+            }
+            head = next;
+        }
+        if (oh == null) {
+            oh = th;
+            ot = tt;
+        } else {
+            ot.next = th;
+            ot = tt;
+        }
+        return oh;
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null) return head;
+        int size = sizeLL(head);
+        k = (k % size);
+        if (k == 0 || k == size) return head;
+        int fwdSize = size - k - 1;
+        ListNode curr = head;
+        while (fwdSize-- > 0) {
+            curr = curr.next;
+        }
+        ListNode secHead = curr.next;
+        ListNode tail = secHead;
+        curr.next = null;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = head;
+        return secHead;
+    }
+
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode temp1 = list1, prev = temp1;
+        for (int i = 1; i <= b + 1; i++) {
+            if (i == a) prev = temp1;
+            temp1 = temp1.next;
+        }
+        prev.next = list2;
+        ListNode tail = list2;
+        while (tail.next != null) tail = tail.next;
+        tail.next = temp1;
+        return list1;
+
+    }
+
 }
